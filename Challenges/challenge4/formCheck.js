dd8664111a14f68528f06567d36e749ea21c1406
mrkiedes <akiedes@vtsd.com>
2024-01-08 13:30:04 -0500

    Form Check Files

--- Challenges/challenge4/formCheck.js
+++ Challenges/challenge4/formCheck.js
@@ -6,5 +6,10 @@ submitButton.addEventListener('click', checkForm);
 
 
 function checkForm(){
+    /*
+        In this method check whether name, email, and phone number have input.
+        If they are blank turn the fields red.  Otherwise do nothing.
+     */
+
     alert("It is hooked up!");
 }
