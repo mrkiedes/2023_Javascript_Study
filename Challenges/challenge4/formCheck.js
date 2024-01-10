6607c9e65287fce038900fda2aa27072ac6af895
mrkiedes <akiedes@vtsd.com>
2024-01-10 12:47:42 -0500

    Updates

--- Challenges/challenge4/formCheck.js
+++ Challenges/challenge4/formCheck.js
@@ -1,15 +1,28 @@
 // Gain Access to form elements
 const submitButton = document.getElementById('submit');
+const nameField = document.getElementById('name');
+const phoneField = document.getElementById('phone');
+const emailField = document.getElementById('email');
+const form = document.getElementById('form');
 
 // Add event listener to submit button
 submitButton.addEventListener('click', checkForm);
-
+form.addEventListener('onsubmit', checkForm);
 
 function checkForm(){
+
     /*
         In this method check whether name, email, and phone number have input.
-        If they are blank turn the fields red.  Otherwise do nothing.
+        If they are blank turn the fields red.  Otherwise, do nothing.
      */
 
-    alert("It is hooked up!");
+    // Check name field
+    if(nameField.value === ''){
+        document.getElementById('lbl_name').style.color = 'red';
+        return false;
+    }else{
+        document.getElementById('lbl_name').style.color = 'black';
+    }
+
+
 }
